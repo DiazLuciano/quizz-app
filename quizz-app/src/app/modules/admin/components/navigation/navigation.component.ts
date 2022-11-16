2b188314bfbf2d63cad88a94e527cf07eeeee6bb
LucianoDiaz <luchodiaz1995@gmail.com>
2022-11-16 02:50:29 -0300

    feat(nav): put user email to label

--- quizz-app/src/app/modules/admin/components/navigation/navigation.component.ts
+++ quizz-app/src/app/modules/admin/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../../../core/auth/services/auth/auth.service';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,12 +9,18 @@ import { AuthService } from '../../../../../core/auth/services/auth/auth.service
 })
 export class NavigationComponent {
 
+  public userEmail: string = '';
+
   /**
    * Constructor.
    *
    * @param _authService
    */
-  constructor(private _authService: AuthService) { }
+  constructor(
+    private _authService: AuthService,
+    private _dashboardSevice: DashboardService) {
+      this.userEmail = this._dashboardSevice.getUserEmail();
+     }
 
   /**
    * This method calls the sign out method from auth service.
---
+++ quizz-app/src/app/modules/admin/services/dashboard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { IUser } from 'src/core/auth/interfaces/auth.interface';
+import { AuthService } from '../../../../core/auth/services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  constructor(
+    private _authService: AuthService
+  ) { }
+
+  public getUserEmail(): string {
+    const user: IUser = this._authService.getUserFromSessionStorage();
+    return user.email;
+  }
+}
